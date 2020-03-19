e949024a0e47a11a3b509235bead0ecb686b4edc
Naomi Jacquet <naomij157@gmail.com>
2020-03-20 00:30:42 +0100

    feat : l'homotopie est symmétrique, réarrangement

--- src/main.lean
+++ src/main.lean
@@ -1,7 +1,8 @@
 --- TODO : 
---  - finir la contractilité de ℝ
+--  - pointer la chose
+--  - retirer des simps et optimiser
 --  - finir l'homotopie comme équivalence
-
+--  - interval semiring ?
 import topology.basic
 import topology.algebra.continuous_functions
 import topology.continuous_on
@@ -9,6 +10,7 @@ import topology.algebra.ordered
 import topology.constructions
 import topology.algebra.ordered
 import data.set.function
+import topology.constructions
 
 import topology.algebra.continuous_functions
 import misc
@@ -19,34 +21,41 @@ open classical
 
 set_option pp.beta true
 
+variable X:Type
+variable [topological_space X] 
+variable [inhabited X] -- on va pointer les applications en default X
 
--- définitions obsolètes
---def is_path (X : Type) [topological_space X] (f : I  -> X ) := continuous f
---def is_loop (X : Type) [topological_space X] (f : I  -> X ) := f(0)=f(1) ∧ (continuous f)
 
 --Définitions par soustype d'un chemin et d'une boucle
 /-- Chemin sur un type X -/
-def path (X : Type) [topological_space X] := {f: I → X // continuous f} 
+def path := {f: I → X // continuous f} 
 /-- Boucle sur un type X -/
-def loop (X : Type) [topological_space X] := {f: I → X // continuous f ∧ f(0)=f(1)} 
+def loop := {f: I → X // continuous f ∧ f(0)=f(1)} 
 
-
+--structure point base ?
 def foo : I -> ℝ := λ x, 0
 def const_0_I : loop ℝ  := ⟨foo, by {split, assume h,  exact continuous_const h, refl}⟩
 
 
 /-- Homotopie de lacets -/
-def loop_homotopy (X : Type) [topological_space X] (f : loop X) (g : loop X) : Prop := 
-    ∃ (H : I × I -> X),  (∀ t, H(0,t) = f.val(t) ∧  H(1,t)=subtype.val g(t)) ∧ (continuous H) 
+def loop_homotopy (f : loop X) (g : loop X) : Prop := 
+    ∃ (H : I × I -> X),  (∀ t, H(0,t) = f.val(t) ∧  H(1,t)=g.val(t)) ∧ (continuous H) 
 
-/-- Compostition d'homotopies -/
-noncomputable def loop_comp (X : Type) [topological_space X] (f : loop X) (g : loop X) : loop X := 
-    ⟨λ t, if t.val≤0.5 then f.val(⟨2*t, sorry⟩) else g.val (⟨ 2*t-1, sorry⟩), sorry⟩
 
-variable X:Type
-variable [topological_space X] 
+/-- Compostition de lacets -/
+noncomputable def loop_comp (f : loop X) (g : loop X) : loop X := 
+    ⟨λ t, if h:t.val≤0.5 then f.val(⟨2*t.val, twotimes t h⟩) 
+    else g.val (⟨ 2*t-1, twotimesminus1 t (em_I t h)⟩), 
+    begin 
+    sorry
+     end⟩
 
-#check (loop_comp ℝ const_0_I const_0_I).val 
+
+
+def loop_inv (f : loop X) : loop X := ⟨λ x:I, f.val(⟨1-x.val, oneminus x⟩ ), 
+        and.intro (continuous.comp f.property.left oneminuscont) (by { simp [f.property.right], sorry })        ⟩
+
+#check subtype
 
 /-- L'homotopie est reflexive -/
 theorem loop_homotopy_refl : reflexive (loop_homotopy X) :=
@@ -66,31 +75,44 @@ begin
     exact continuous_snd,
 end
 
+
+
 /-- L'homotopie est symmétrique -/
 theorem loop_homotopy_symm : symmetric (loop_homotopy X) :=
 begin
     assume f g,
     intro h1, 
     cases h1 with H hH,
-    let H2 : I × I -> X := λ x, H(⟨1-x.1, sorry⟩, x.2),
+    let H2 : I × I -> X := λ x, H(⟨1-x.1.val, oneminus x.1⟩, x.2),
     use H2,
-    have stupid : ↑(0 : I) = (0 : ℝ ), from rfl,
-    have stupid2 : (1 : ℝ) + -↑(0 : I) = (1 : ℝ),  rw stupid, ring,
     split,
-        intros,
-        split,
-        simp *,
-        sorry,
-        sorry,
-        sorry,
-        --calc
-          --  H (⟨1 + -↑((0, t).fst), _⟩, (0, t).snd) = H (⟨1 + -↑((0, t).fst), _⟩, t) : rfl
-            --                            ...        = H (⟨1 + -↑((0, t).fst), _⟩, t)  : rfl
-              --                          ...        = H (⟨1 + -↑0, _⟩, t) : rfl
-                --                        ...        = H (1, t) : sorry
-                  --                      ...        = g.val t : by rw loop_homotopy.property
+
+    -- fixe les extrémités
+    intros,
+    split,
+    simp *,
+    simp [ coe_of_0],
+    rw <- (hH.1 t).2,
+    congr,
+    simp *,
+    simp [one_minus_one_coe],
+    rw <- (hH.1 t).1,
+    congr,
+
+    -- continuité
+    apply continuous.comp,
+    exact hH.2,
+    apply continuous.prod_mk,
+    apply continuous_subtype_mk,
+    apply continuous.sub,
+    apply continuous_const,
+    apply continuous.comp,
+    exact continuous_subtype_val,
+    exact continuous_fst,
+    exact continuous_snd,
 
 end
+
 
 /-- L'homotopie est transitive -/
 theorem loop_homotopy_trans : transitive (loop_homotopy X) :=
@@ -121,46 +143,3 @@ notation `[` f `|` X `]` := reduce_homotopy X f
 #check [const_0_I | ℝ] -- classe d'homotopie de l'application constante sur ℝ 
 
 
-
-
--- homotopie de fonctions
-def is_homotopy (X : Type) [topological_space X] (f : X -> X) (g : X -> X) (H : X × I -> X) := 
-         (∀ t, H(t,0)=f(t)) ∧ (∀ t, H(t,1)=g(t)) ∧ (continuous H) 
-         
-def const_0: ℝ -> ℝ  := λ x, 0
-
-class contractible_space (X : Type) [topological_space X] :=
-    (H : X × I → X)
-    (const : X)
-    (H_is_homotopy : is_homotopy X (λ x, const) id H)
- 
-def H : ℝ × I -> ℝ := λ x, (1-x.2.val) * const_0(x.1) + x.2.val * id(x.1)
-
-lemma is_continuous_H : continuous H := 
-begin
-    rw H,
-    rw const_0,
-    simp,
-    sorry,
-end
-
-
-theorem contractible_R : is_homotopy ℝ const_0 id H := 
-begin
-    split,
-        intros,
-        rw [H ,const_0],
-        simp,
-            left,
-            refl,
-        split,
-            intros,
-            rw [H ,const_0],
-            simp,
-            have h : (1 : I).val = 1, from rfl,
-            rw h,
-            ring,
-        exact is_continuous_H,
-end
-
-noncomputable instance : contractible_space ℝ := ⟨H, 0, contractible_R⟩ 
--- src/misc.lean
+++ src/misc.lean
@@ -1,10 +1,55 @@
 import data.set.intervals.basic
+import data.real.basic
 import topology.instances.real
+import tactic.suggest
 
 open set 
 
 --  Propriétés de base sur l'ensemble [0,1]
-def I := Icc (0 : ℝ) (1 : ℝ)
-instance : has_zero I := ⟨⟨0, and.intro (le_refl 0) (zero_le_one)⟩⟩
+
+abbreviation I := Icc (0 : ℝ) (1 : ℝ)
+
+
+instance : has_zero I := ⟨⟨ (0 : ℝ), and.intro (le_refl 0) (zero_le_one)⟩⟩
 instance : has_one I := ⟨⟨1, and.intro (zero_le_one) (le_refl 1)⟩⟩
-instance : has_coe I ℝ := ⟨λ x,x⟩
+noncomputable def real_to_I (x : ℝ) : I := if h : x≥0 ∧ x≤1 then ⟨x, h⟩ else (0 : I)
+instance : has_coe I ℝ := ⟨subtype.val⟩
+instance : has_le I := ⟨λ r s, (r:ℝ) ≤ s⟩
+instance : inhabited I := ⟨0⟩ 
+
+--instance : has_div I := ⟨λ x y, real_to_I (x/y) ⟩
+noncomputable instance : has_sub I := ⟨λ a b, real_to_I (a-b)⟩ 
+
+lemma oneminus (t : I) : 1-t.val ≥ 0 ∧ 1-t.val ≤ 1 :=
+begin
+  simp,
+  split,
+  exact t.property.right,
+  ring,
+  apply add_le_iff_nonpos_left.mpr,
+  apply neg_le.mp,
+  simp,
+  exact t.property.1,
+end
+
+lemma oneminuscont : continuous (λ t:I, (⟨1-t, oneminus t⟩:I)) := sorry
+
+lemma twotimes (t:I) : t.val ≤ 0.5 -> 2*t.val ≥ 0 ∧ 2*t.val ≤ 1  := 
+begin
+    intro h,
+    simp,
+    sorry,
+end
+
+lemma twotimesminus1 (t:I) : t.val > 0.5 -> 2*t.val-1 ≥ 0 ∧ 2*t.val-1 ≤ 1  := 
+begin
+    intro h,
+    simp,
+    sorry,
+end
+
+lemma em_I (t : I) :¬t.val ≤ 1 / 2 -> t.val > 1 / 2 := sorry
+
+
+@[simp] lemma coe_of_0 : (0 : I).val = (0 : ℝ ) := rfl
+@[simp] lemma one_minus_one_coe : (1:ℝ) - (1 : I).val = (0 : ℝ ) := sub_self 1
